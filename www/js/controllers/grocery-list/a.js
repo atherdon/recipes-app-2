2f7f58b4971472e453651decd54d943e106754d2
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-03-12 20:13:01 -0700

    updates
    styles, new folder structure, moved recipes templates from free branch,
    removed not important templates

---
+++ www/js/controllers/grocery-list/a.js
@@ -0,0 +1,62 @@
+angular.module('starter.controllers', ['app.services'])
+
+
+.controller('groceryListSettingsCtrl', function($scope, $stateParams, Settings) {
+
+
+})
+
+.controller('groceryListCtrl', function($scope, $stateParams, groceryCategory) {
+
+//@TODO remove getting info and move it to services.
+//Looks awful right now. Also remove not necessary variables from constructor and method
+    
+    groceryCategory.getCategories( function(data){
+
+      console.log(data);
+
+      $scope.categories = data;
+
+    });
+
+})
+
+.controller('recipesCalendarCtrl', function($scope, $stateParams, Calendar) {
+
+  var Cal = new Calendar();
+
+  Cal.fetch().then(function(){
+
+    
+    $scope.week = Cal.week;
+    
+  });
+
+   
+})
+
+.controller('categoryCtrl', function($scope, $stateParams, Grocery) {
+
+
+//    @TODO create a service method for getting only values(array) for only one category.
+//    get needed category id from service method with passing category id from view
+// remove pages like grocery food categories and create a groceryCategoryList with passing data to it
+
+// @TODO add page for flushing checkboxes
+
+
+
+  // console.log( $stateParams.categoryId );
+
+  var GL = new Grocery( $stateParams.categoryId );
+
+  GL.getCategory().then(function(){
+
+    // console.log( GL );
+    
+    $scope.title       = GL.name;
+    $scope.ingredients = GL.items;
+  });
+
+
+})
---
+++ www/js/controllers/recipe/a.js
@@ -0,0 +1,69 @@
+angular.module('starter.controllers', ['app.services'])
+
+
+.controller('ingredientsCtrl', function($scope, $stateParams, Ingredients) {
+
+  var List = new Ingredients();
+
+  List.fetch().then(function(){
+
+    
+    $scope.array = List.list;
+    
+  });
+
+})
+
+
+
+.controller('directionsCtrl', function($scope, $stateParams, Directions) {
+
+  var List = new Directions();
+
+  List.fetch().then(function(){
+
+    
+    $scope.array = List.list;
+    
+  });
+
+})
+
+.controller('singleRecipeCtrl', function($scope, $stateParams, Recipe) {
+
+  var recipe = new Recipe( $stateParams.recipeId );
+
+  recipe.fetch().then(function(){
+
+    // check if recipe have all important information. This is not completed. Must be.
+    if( recipe ){
+
+      // $scope.title       = GL.name;
+      // $scope.ingredients = GL.items;
+
+    } else {
+
+      $scope.message = "Nukhua netu dannuh y recepta etogo";
+    }
+
+    // console.log( recipe );
+    $scope = recipe;
+    // $scope.title       = GL.name;
+    // $scope.ingredients = GL.items;
+
+  });
+
+})
+
+.controller('styledSingleRecipeCtrl', function($scope, $stateParams, Recipe) {
+
+})
+
+.controller('uXSingleRecipeCtrl', function($scope, $stateParams, Recipe) {
+
+})
+
+.controller('uXSingleRecipe2Ctrl', function($scope, $stateParams, Recipe) {
+
+
+})

e9ba4b04a8652188d01cfe41f285319d467085aa
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-03-31 04:44:25 -0700

    intro preparation

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -184,4 +184,32 @@ angular.module('starter.controllers', ['app.services'])
 
 })
 
+
+.controller('introCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+// ../js/api/json/intro.json
+
+    // Called to navigate to the main app
+//      $scope.startApp = function() {
+//        $state.go('login');
+//      };
+//      $scope.next = function() {
+//        $ionicSlideBoxDelegate.next();
+//      };
+//      $scope.previous = function() {
+//        $ionicSlideBoxDelegate.previous();
+//      };
+//
+//      // Called each time the slide changes
+//      $scope.slideChanged = function(index) {
+//        $scope.slideIndex = index;
+//      };
+
+
+
+}])
+
 ;

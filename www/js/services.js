ce97effabe2b7444ddd918327eaac81db2a8b1fa
Arthur Tkachenko <atherdon@gmail.com>
2017-04-01 01:33:23 -0700

    Update services.js

--- www/js/services.js
+++ www/js/services.js
@@ -291,61 +291,7 @@ angular.module('app.services', [])
 
 })
 
-.service('groceryCategory', ['$http', function( $http ){
 
-   
-
-    return {
-            get: function() {
-                
-                return $http.get('/js/api/json/grocery.json');
-            },
-
-            getCategory: function(callback) {
-                
-
-
-                $http.get('/js/json/grocery.json').then(function(response) {
-                    
-                    angular.forEach( response.data, function(value, key){
-
-                        if( value.category_id == "3" ){
-
-                            callback(value);
-                            
-                        }
-
-                    });
-                    
-
-                });
-                
-
-            },
-
-            getCategories: function(callback){
-                
-                var array = [];
-                
-                $http.get('/js/json/grocery.json').then(function(response) {
-                    
-                    angular.forEach( response.data, function(value, key){
-
-                        array.push({
-                            "category_id" : value.category_id,
-                            "name"        : value.name
-                        });
-
-
-                    });
-                    callback(array);
-
-                });
-            }
-
-    }
-
-}])
 
 
 

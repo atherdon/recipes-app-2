7e86481c002b2ad1bf2ae9368d63205394043d27
Arthur Tkachenko <atherdon@gmail.com>
2017-04-03 17:44:56 -0700

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,13 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'app.services', 'app.directives'])
+angular.module('starter', [
+  'ionic',
+  'starter.controllers',
+  'app.services',
+  'app.directives',
+  'app.routes'
+])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -21,4 +27,4 @@ angular.module('starter', ['ionic', 'starter.controllers', 'app.services', 'app.
     }
   });
 })
-;
+
